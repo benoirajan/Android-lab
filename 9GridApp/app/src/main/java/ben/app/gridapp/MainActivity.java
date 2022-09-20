718d7122cdab5af796b1dfc53756d8956fb04dee
unknown <Administrator@MCALABII-17.MCALABIISERVER.ORG>
2022-09-20 11:04:52 +0530

    change

--- 9GridApp/app/src/main/java/ben/app/gridapp/MainActivity.java
+++ 9GridApp/app/src/main/java/ben/app/gridapp/MainActivity.java
@@ -1,13 +1,23 @@
 package ben.app.gridapp;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
+    TextView operand;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        operand =findViewById(R.id.op_text);
+    }
+
+    public void num(View v){
+        Button b = (Button) v;
+        operand.append(b.getText());
     }
 }
