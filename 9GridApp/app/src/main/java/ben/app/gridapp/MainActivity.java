044da3bf091e53e54c375a972ef997c5e880f38b
unknown <Administrator@MCALABII-17.MCALABIISERVER.ORG>
2022-09-22 11:55:18 +0530

    calc grid

--- 9GridApp/app/src/main/java/ben/app/gridapp/MainActivity.java
+++ 9GridApp/app/src/main/java/ben/app/gridapp/MainActivity.java
@@ -8,16 +8,94 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-    TextView operand;
+    private TextView op1Text, op2Text, resultTxt, opr;
+    private int opId = R.id.add;
+    private double op1, op2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        operand =findViewById(R.id.op_text);
+        opr = op1Text = findViewById(R.id.op_text);
+        op2Text = findViewById(R.id.op2);
+        resultTxt = findViewById(R.id.result);
     }
 
-    public void num(View v){
+    public void num(View v) {
         Button b = (Button) v;
-        operand.append(b.getText());
+        if (b.getText().toString().equals(".") && opr.getText().toString().contains("."))
+            return;
+        opr.append(b.getText());
+    }
+
+    public void operator(View v) {
+        switch (v.getId()) {
+            case R.id.ac:
+                clear();
+                break;
+            case R.id.equal:
+                findResult();
+                break;
+            case R.id.add:
+            case R.id.sub:
+            case R.id.division:
+            case R.id.multiply:
+                opId = v.getId();
+                performCalc(v.getId());
+                break;
+        }
+    }
+
+    private void performCalc(int id) {
+        if (op1Text.getText().toString() == null || op1Text.getText().toString().isEmpty())
+            return;
+
+        if (op2Text.getText().toString() == null || op2Text.getText().toString().isEmpty())
+            op1 = Double.parseDouble(op1Text.getText().toString());
+        switch (id) {
+            case R.id.add:
+                op1Text.append(" +");
+                break;
+            case R.id.sub:
+                op1Text.append(" -");
+                break;
+            case R.id.division:
+                op1Text.append(" ÷");
+                break;
+            case R.id.multiply:
+                op1Text.append(" ×");
+                break;
+        }
+        opr = op2Text;
+    }
+
+    private void findResult() {
+        if (op2Text.getText().toString() == null || op2Text.getText().toString().isEmpty())
+            return;
+        op2 = Double.parseDouble(op2Text.getText().toString());
+        double res = 0;
+        switch (opId) {
+            case R.id.add:
+                res = op1 + op2;
+                break;
+            case R.id.sub:
+                res = op1 - op2;
+                break;
+            case R.id.division:
+                res = op1 / op2;
+                break;
+            case R.id.multiply:
+                res = op1 * op2;
+                break;
+        }
+        resultTxt.setText("= " + res);
+        opr = op1Text;
+    }
+
+    private void clear() {
+        op1Text.setText("");
+        op2Text.setText("");
+        resultTxt.setText("");
+        opr = op1Text;
     }
 }
