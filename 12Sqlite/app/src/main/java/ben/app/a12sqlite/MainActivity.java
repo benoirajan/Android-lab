f90099a607b98113b16f00defffb31dbcbad3301
unknown <Administrator@MCALABII-17.MCALABIISERVER.ORG>
2022-11-03 11:50:52 +0530

    sqlite

--- 12Sqlite/app/src/main/java/ben/app/a12sqlite/MainActivity.java
+++ 12Sqlite/app/src/main/java/ben/app/a12sqlite/MainActivity.java
@@ -1,7 +1,10 @@
 package ben.app.a12sqlite;
 
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.BatteryManager;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -12,12 +15,30 @@ import ben.app.a12sqlite.db.DbUtil;
 
 public class MainActivity extends AppCompatActivity {
     TextView tv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tv =findViewById(R.id.text);
+        tv = findViewById(R.id.text);
+        findViewById(R.id.button).setOnClickListener(v -> {
+            recordBattery();
+            showDb();
+        });
         showDb();
+    }
+
+    private void recordBattery() {
+        IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        Intent batteryStatus = registerReceiver(null, ifilter);
+
+        int level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+        int scale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+
+        float batteryPct = level * 100 / (float)scale;
+
+        SQLiteDatabase db = new DbUtil(this).getReadableDatabase();
+        DbUtil.insertBat(db, cal, 30f, 45);
     }
 
     private void showDb() {
@@ -42,10 +63,10 @@ public class MainActivity extends AppCompatActivity {
             double temperature = cursor.getDouble(
                     cursor.getColumnIndexOrThrow(BatteryInfo.TEMPERATURE));
 
-            tv.append(String.format("{ %d, %s, %f },\n",percentage,time,temperature));
+            tv.append(String.format("{ %d, %s, %f },\n", percentage, time, temperature));
         }
         cursor.close();
-        DbUtil.insertBat(db,cal,30f,45);
+
     }
 
 
