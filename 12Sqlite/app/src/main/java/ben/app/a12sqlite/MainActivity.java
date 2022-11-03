44ae548a39932e0af11e0d82951271fd58f728ea
unknown <Administrator@MCALABII-17.MCALABIISERVER.ORG>
2022-11-03 11:30:45 +0530

    sqlite

--- 12Sqlite/app/src/main/java/ben/app/a12sqlite/MainActivity.java
+++ 12Sqlite/app/src/main/java/ben/app/a12sqlite/MainActivity.java
@@ -1,14 +1,52 @@
 package ben.app.a12sqlite;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import ben.app.a12sqlite.db.BatteryInfo;
+import ben.app.a12sqlite.db.DbUtil;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv =findViewById(R.id.text);
+        showDb();
     }
+
+    private void showDb() {
+        tv.setText("Percent, Time, Temperature\n");
+        SQLiteDatabase db = new DbUtil(this).getReadableDatabase();
+        String cal = "2022-11-03";
+        String sql = "SELECT *  FROM " +
+                BatteryInfo.TABLE_NAME + " " +
+                "WHERE " + BatteryInfo.TIME +
+                " = date('" + cal + "') " +
+                "ORDER BY " + BatteryInfo.TIME + " DESC";
+        Cursor cursor = db.rawQuery(sql, null);
+
+//        data = new String[cursor.getCount()][DailyUtil.COUNT];
+//        amount = new double[cursor.getCount()];
+//        ids = new long[cursor.getCount()];
+        while (cursor.moveToNext()) {
+            long percentage = cursor.getLong(
+                    cursor.getColumnIndexOrThrow(BatteryInfo.PERCENTAGE));
+            String time = cursor.getString(
+                    cursor.getColumnIndexOrThrow(BatteryInfo.TIME));
+            double temperature = cursor.getDouble(
+                    cursor.getColumnIndexOrThrow(BatteryInfo.TEMPERATURE));
+
+            tv.append(String.format("{ %d, %s, %f },\n",percentage,time,temperature));
+        }
+        cursor.close();
+        DbUtil.insertBat(db,cal,30f,45);
+    }
+
+
 }
---
+++ 12Sqlite/app/src/main/java/ben/app/a12sqlite/db/DbUtil.java
@@ -0,0 +1,46 @@
+package ben.app.a12sqlite.db;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DbUtil extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "batteryInfo";
+    private static final int DATABASE_VERSION = 2;
+
+    public static final String CREATE_TABLE = "CREATE TABLE " + BatteryInfo.TABLE_NAME +
+            " (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
+            BatteryInfo.TIME + " TEXT  NOT NULL, " +
+            BatteryInfo.TEMPERATURE + " REAL NOT NULL, " +
+            BatteryInfo.PERCENTAGE + " INTEGER NOT NULL)";
+
+
+    public DbUtil(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_TABLE);
+    }
+
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+
+    public static void insertBat(SQLiteDatabase db, String time, float temperature, int percent) {
+        ContentValues values = new ContentValues();
+        values.put(BatteryInfo.PERCENTAGE, percent);
+        values.put(BatteryInfo.TEMPERATURE, temperature);
+        values.put(BatteryInfo.TIME, time);
+
+        if (db.insert(BatteryInfo.TABLE_NAME, null, values) == -1)
+            throw new SQLiteException("Error on query : " + values.toString());
+    }
+}
